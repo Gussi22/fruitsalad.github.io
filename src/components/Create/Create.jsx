7ef671e88468f51f27588a235a4e65f295ac68d8
Gussi22 <samurai.adrian22@gmail.com>
2023-07-16 16:47:31 +0800

    Adding download button for optional commands

--- src/components/Create/Create.jsx
+++ src/components/Create/Create.jsx
@@ -1,29 +1,35 @@
 import React from "react";
 import CreateSaladRequired from "../../Downloads/CreateSalad_Required.zip"
+import CreateSaladOptional from "../../Downloads/CreateSalad_Optional.zip"
 
 export const Create = (props) => {
     return (
         <div className="createHome">
             <h2>CreateSalad</h2>
-            <label>Required Mods</label>
-            <a
-            href={CreateSaladRequired}
-            download="CreateSalad_Required"
-            target="_blank"
-            rel="noopener noreferrer"
-            >
-                <button className="link-btn">.zip</button>
-            </a>
-            <h2>CreateSalad</h2>
-            <label>Required Mods</label>
-            <a
-            href={CreateSaladRequired}
-            download="CreateSalad_Required"
-            target="_blank"
-            rel="noopener noreferrer"
-            >
-                <button className="link-btn">.zip</button>
-            </a>
+            <div className="mods-container">
+                <div className="required">
+                    <label>Required Mods</label>
+                    <a
+                    href={CreateSaladRequired}
+                    download="CreateSalad_Required"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    >
+                        <button className="link-btn">.zip</button>
+                    </a>
+                </div>
+                <div className="required">
+                    <label>Optional Mods</label>
+                    <a
+                    href={CreateSaladOptional}
+                    download="CreateSalad_Optional"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    >
+                        <button className="link-btn">.zip</button>
+                    </a>
+                </div>
+            </div>
         </div>
     )
 }
