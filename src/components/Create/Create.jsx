0454f414cd153dad9bf7d1f11ba2f07725484250
Gussi22 <samurai.adrian22@gmail.com>
2023-07-16 16:18:48 +0800

    Fixing css

--- src/components/Create/Create.jsx
+++ src/components/Create/Create.jsx
@@ -4,30 +4,26 @@ import CreateSaladRequired from "../../Downloads/CreateSalad_Required.zip"
 export const Create = (props) => {
     return (
         <div className="createHome">
-            <div>
-                <h2>CreateSalad</h2>
-                <label>Required Mods</label>
-                <a
-                href={CreateSaladRequired}
-                download="CreateSalad_Required"
-                target="_blank"
-                rel="noopener noreferrer"
-                >
-                    <button className="link-btn">.zip</button>
-                </a>
-            </div>
-            <div>
-                <h2>CreateSalad</h2>
-                <label>Required Mods</label>
-                <a
-                href={CreateSaladRequired}
-                download="CreateSalad_Required"
-                target="_blank"
-                rel="noopener noreferrer"
-                >
-                    <button className="link-btn">.zip</button>
-                </a>
-            </div>
+            <h2>CreateSalad</h2>
+            <label>Required Mods</label>
+            <a
+            href={CreateSaladRequired}
+            download="CreateSalad_Required"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+                <button className="link-btn">.zip</button>
+            </a>
+            <h2>CreateSalad</h2>
+            <label>Required Mods</label>
+            <a
+            href={CreateSaladRequired}
+            download="CreateSalad_Required"
+            target="_blank"
+            rel="noopener noreferrer"
+            >
+                <button className="link-btn">.zip</button>
+            </a>
         </div>
     )
 }
