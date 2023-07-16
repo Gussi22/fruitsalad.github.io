145aa353559224d0837c73aa2dc8559acd81a6a2
Gussi22 <samurai.adrian22@gmail.com>
2023-07-16 16:10:20 +0800

    Fixing css

--- src/App.js
+++ src/App.js
@@ -3,17 +3,9 @@ import React, { useState } from 'react';
 import Create from './components/Create/Create';
 
 function App() {
-  const [currentForm, setCurrentForm] = useState('login');
-
-  const toggleForm = (formName) => {
-    setCurrentForm(formName);
-  }
-
   return (
     <div className='App'>
-      <div>
-        <Create />
-      </div>
+      <Create />
     </div>
   );
 }
--- src/components/Create/Create.jsx
+++ src/components/Create/Create.jsx
@@ -4,16 +4,30 @@ import CreateSaladRequired from "../../Downloads/CreateSalad_Required.zip"
 export const Create = (props) => {
     return (
         <div className="createHome">
-            <h2>CreateSalad</h2>
-            <label>Required Mods</label>
-            <a
-             href={CreateSaladRequired}
-             download="CreateSalad_Required"
-             target="_blank"
-             rel="noopener noreferrer"
-            >
-                <button className="link-btn">.zip</button>
-            </a>
+            <div>
+                <h2>CreateSalad</h2>
+                <label>Required Mods</label>
+                <a
+                href={CreateSaladRequired}
+                download="CreateSalad_Required"
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                    <button className="link-btn">.zip</button>
+                </a>
+            </div>
+            <div>
+                <h2>CreateSalad</h2>
+                <label>Required Mods</label>
+                <a
+                href={CreateSaladRequired}
+                download="CreateSalad_Required"
+                target="_blank"
+                rel="noopener noreferrer"
+                >
+                    <button className="link-btn">.zip</button>
+                </a>
+            </div>
         </div>
     )
 }
